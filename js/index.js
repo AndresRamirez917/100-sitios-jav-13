a62107c841063b7662cd6621086154c7666b1f2c
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-03 14:31:40 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -15,4 +15,49 @@ async function getData() {
         }
     });
 }
+
+
+const btn_validar = document.getElementById('btn-validar')
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const arrMessages = ["nombre", "email", "mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i++){
+        for(i = 0; i < arrMessages.length; i++){
+            if(arr[i].value == ""){
+                swal({
+                    title: `El campo ${arrMessages[i]} no puede estar vacío`,
+                    icon: "error",
+                     })
+                     return false;
+            }
+        }
+        if(!emailValido(email.value)){
+            swal({
+                title: `El formato del ${arrMessages[1]} no es correcto`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+    swal({
+        title: `Los datos fueron enviados satisfactoriamente`,
+        icon: "success",
+         })
+    nombre.value = "";
+    email.value = "";
+    mensaje.value = "";
+    return true;
+
+}
+
+const emailValido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+btn_validar.addEventListener("click", validar)
 getData()
